e3499399fa4ce59e5f79360e3b587e486c9e2d0f
Antanid <dbalakleenko1@gmail.com>
2023-08-29 18:03:17 +0300

    lazy fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import "./App.css";
 import { Route, Routes, useLocation } from "react-router";
 import Footer from "./components/Footer/Footer";
 import Menu from "./components/Menu/Menu";
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 
-const MainPage = lazy(() => import ("./page/MainPage/MainPage"));
-const SingleCoinPage = lazy(() => import ("./page/MainPage/MainPage"));
-const FavoritePage = lazy(() => import ("./page/FavoritePage/FavoritePage"));
-
-
+const MainPage = lazy(() => import("./page/MainPage/MainPage"));
+const SingleCoinPage = lazy(() => import("./page/MainPage/MainPage"));
+const FavoritePage = lazy(() => import("./page/FavoritePage/FavoritePage"));
 
 function App() {
   const singlId = useLocation();
@@ -16,9 +14,30 @@ function App() {
     <div className="App">
       <Menu />
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path={`/single_coin_${singlId.pathname.split('_').reverse()[0]}`} element={<SingleCoinPage />} />
-        <Route path="favorite-page" element={<FavoritePage/>}/>
+        <Route
+          path="/"
+          element={
+            <Suspense>
+              <MainPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path={`/single_coin_${singlId.pathname.split("_").reverse()[0]}`}
+          element={
+            <Suspense>
+              <SingleCoinPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="favorite-page"
+          element={
+            <Suspense>
+              <FavoritePage />
+            </Suspense>
+          }
+        />
       </Routes>
       <Footer />
     </div>
